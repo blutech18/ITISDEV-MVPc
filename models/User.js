21c02c593cfbffebb2ccb1cf18057d719b28f70d
henryagunanne <henryagunanne@gmail.com>
2026-03-07 22:11:46 +0800

    ...

--- models/User.js
+++ models/User.js
@@ -57,7 +57,7 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next(); // Only hash if password is new or modified
     try {
-      const salt = await bcrypt.genSalt(10);
+      const salt = await bcrypt.genSalt(12);
       this.password = await bcrypt.hash(this.password, salt);
       next();
     } catch (err) {
